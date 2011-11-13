eff8cc9d5b5bc8eb26d2ec5ffddaea592c6110e7
Nathan Jones <thenathanjones@gmail.com>
2011-11-13 17:31:57 +0800

    Initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvdlibrary::Application.config.secret_token = '4a5011392a4fe5ab56c937099802f87e166e52749a4b7ffb04b69c93b4fb9a963ea7bafdd8ef976b9d74e4f7769583cdbac2aa148eb54157e780856b0677fb4b'
